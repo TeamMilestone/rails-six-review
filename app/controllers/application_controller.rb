f6f07211d55cc7dd42ad1e9857f513bfd2d9142c
이원섭wonsup Lee <onesup.lee@gmail.com>
2019-12-15 05:00:13 +0900

    redirect to dashboard when admin login.
    관리자가 로그인하면 관리자 대시보드 root 로 이동.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)    
+    if resource.is_a? StaffUser
+      locate = admin_root_path
+    else
+      locate = root_path
+    end    
+    stored_location_for(resource) || locate
+  end
 end
